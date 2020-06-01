38eb5b5c94fcbea71a914bd14e380b8cdda53fca
anlzou <599502931@qq.com>
2020-06-02 00:11:17 +0800

    feat(M0009):update M0009*.c

--- code/M0009_ConcatenateString.c
+++ code/M0009_ConcatenateString.c
@@ -2,7 +2,7 @@
  * @Date        : 2020-06-01 23:51:16
  * @LastEditors : anlzou
  * @Github      : https://github.com/anlzou
- * @LastEditTime: 2020-06-02 00:09:47
+ * @LastEditTime: 2020-06-02 00:10:42
  * @FilePath    : \algorithm\code\M0009_ConcatenateString.c
  * @Describe    :
  */
@@ -14,8 +14,8 @@ int main() {
     char *str2 = " a clock";
     char *str3 = (char *)malloc(sizeof(str1) + sizeof(str2) + 4);
 
-    printf("len str1: %d\n", sizeof(str1));
-    printf("len str2: %d\n", sizeof(str2));
+    printf("len str1: %d\n", sizeof(str1));  // 8
+    printf("len str2: %d\n", sizeof(str2));  // 8
     int mun = 8;
     sprintf(str3, "%s%d%s", str1, mun, str2);
     printf("%s", str3);
