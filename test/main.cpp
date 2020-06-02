971f12aeb2085d5f85d1538cdbfe0795175440ac
anlzou <599502931@qq.com>
2020-06-02 10:17:54 +0800

    test(test/main.cpp):test __**__

--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,23 @@
-/***
- * @Date        : 2020-05-05 19:11:16
- * @LastEditors : anlzou
- * @Github      : https://github.com/anlzou
- * @LastEditTime: 2020-05-21 16:34:29
- * @FilePath    : \algorithm\test\main.cpp
- * @Describe    :
- */
+
 
 #include "./../code/L0003_LongestSubstring.cpp"
 using namespace std;
 
 int main() {
-    Solution s;
-    string result;
-    string str = "abbcdefgegcsgcasse";
-    result = s.no_redup(str);
-    cout << result << endl;
+    // Solution s;
+    // string result;
+    // string str = "abbcdefgegcsgcasse";
+    // result = s.no_redup(str);
+    // cout << result << endl;
+
+    /**
+     * __LINE__ 和 __FILE__ 变量表示正在处理的当前行和当前文件.
+     * __DATE__ 变量表示当前日期,格式为month/day/year(月/日/年).
+     * __TIME__ 变量描述当前的时间,格式为hour:minute:second(时:分:秒).
+     */
+    cout << __FILE__ << endl;
+    cout << __LINE__ << endl;
+    cout << __DATE__ << endl;
+    cout << __TIME__ << endl;
     return 0;
 }
